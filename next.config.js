6158a26bc0b1e450f4aabe6dd608a75c9740f312
NoahR02 <noahreppert95@protonmail.com>
2023-12-28 23:21:09 -0500

    Use .webp.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
 const nextConfig = {
     // Export mode breaks links on dev, so only turn on export mode when we are on prod.
     output: process.env.NODE_ENV === 'development' ? 'standalone' : 'export',
+    images: {
+        formats: ['image/webp'],
+    },
 }
 
 module.exports = nextConfig
